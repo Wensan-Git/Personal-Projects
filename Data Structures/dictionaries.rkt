ae1a1beab04366038c43c69f3cd7fcdb63dcb953
Wensan-Git <85974906+Wensan-Git@users.noreply.github.com>
2022-09-11 23:06:51 -0400

    Update dictionaries.rkt

--- Data Structures/dictionaries.rkt
+++ Data Structures/dictionaries.rkt
@@ -1,17 +1,5 @@
 #lang dssl2
 
-# HW3: Dictionaries
-#
-# ** You must work on your own for this assignment. **
-
-
-let eight_principles = ["Know your rights.", "Acknowledge your sources.",
-"Protect your work.",
-"Avoid suspicion.",
-"Do your own work.",
-"Never falsify a record or permit another person to do so.",
-"Never fabricate data, citations, or experimental results.",
-"Always tell the truth when discussing your work with your instructor."]
 
 import sbox_hash
 
