4c2e22ea53a5ab7f43041ef3bb60ebe50efede69
Wensan-Git <85974906+Wensan-Git@users.noreply.github.com>
2022-09-11 23:06:31 -0400

    Update binheap.rkt

--- Data Structures/binheap.rkt
+++ Data Structures/binheap.rkt
@@ -1,15 +1,5 @@
 #lang dssl2
-let eight_principles = ["Know your rights.", "Acknowledge your sources.",
-"Protect your work.",
-"Avoid suspicion.",
-"Do your own work.",
-"Never falsify a record or permit another person to do so.",
-"Never fabricate data, citations, or experimental results.",
-"Always tell the truth when discussing your work with your instructor."]
 
-# HW5: Binary Heaps
-#
-# ** You must work on your own for this assignment. **
 
 interface PRIORITY_QUEUE[X]:
     # Returns the number of elements in the priority queue.
