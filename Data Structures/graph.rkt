77db559b5e86e8923aadc210a4c948107fe12267
Wensan-Git <85974906+Wensan-Git@users.noreply.github.com>
2022-09-11 23:07:10 -0400

    Update graph.rkt

--- Data Structures/graph.rkt
+++ Data Structures/graph.rkt
@@ -1,15 +1,5 @@
 #lang dssl2
 
-let eight_principles = ["Know your rights.", "Acknowledge your sources.",
-"Protect your work.",
-"Avoid suspicion.",
-"Do your own work.",
-"Never falsify a record or permit another person to do so.",
-"Never fabricate data, citations, or experimental results.",
-"Always tell the truth when discussing your work with your instructor."]
-# HW4: Graph
-#
-# ** You must work on your own for this assignment. **
 
 import cons
 import 'hw4-lib/dictionaries.rkt'
